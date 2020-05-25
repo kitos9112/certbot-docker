3c136e23d1f5d64f4d70197822be40cc8426ef57
Marcos Soutullo <marcos.soutullo91@gmail.com>
2020-05-25 21:18:06 +0100

    Initial commit  - Add a generic & simple build script  - Segregate core and plugins certbot Dockerfiles

--- build.sh
+++ build.sh
@@ -1,63 +1,85 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+trap 1 2 3 6
 
-# This script builds certbot docker and certbot dns plugins docker against a given release version of certbot.
-# The build is done following the environment used by Dockerhub to handle its autobuild feature, and so can be
-# used as a pre-deployment validation test.
+# This script builds certbot docker and all certbot dns plugin docker against the latest release of Certbot inside 32-bit ARMv7 architectures
+# Usage: ./build.sh
 
-# Usage: ./build.sh [VERSION]
-#   with [VERSION] corresponding to a released version of certbot, like `v0.34.0`
+if [[ $(uname -m) != 'armv7l' ]]; then echo "This script only runs within ARMv7 32-bit systems"; exit 1; fi
 
-trap Cleanup 1 2 3 6
-
-Cleanup() {
-    if [ ! -z "$WORK_DIR" ]; then
-        rm -rf "$WORK_DIR"/core/qemu-*-static || true
-        rm -rf "$WORK_DIR"/plugin/qemu-*-static || true
-    fi
-    popd 2> /dev/null || true
+BuildAndPushCore() { 
+    # Fetch Certbot version on 1st positional argument
+    CERTBOT_VERSION="$1"
+    # Fetch Docker repo name on 2nd positional argument
+    DOCKER_REPO="$2" 
+    # Build certbot core Docker image
+    docker build \
+        --build-arg CERTBOT_VERSION="${CERTBOT_VERSION}" \
+        -f source/core-armv7l/Dockerfile \
+        -t "${DOCKER_REPO}:v${CERTBOT_VERSION}" \
+        .
+    docker push "${DOCKER_REPO}:v${CERTBOT_VERSION}"
+    docker tag "${DOCKER_REPO}:v${CERTBOT_VERSION}" "${DOCKER_REPO}:latest"
+    docker push "${DOCKER_REPO}:latest"
 }
 
-Build() {
-    DOCKER_REPO="$1"
-    CERTBOT_VERSION="$2"
-    CONTEXT_PATH="$3"
-    DOCKERFILE_PATH="$CONTEXT_PATH/Dockerfile"
-    DOCKER_TAG="$CERTBOT_VERSION"
-    pushd "$CONTEXT_PATH"
-        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/pre_build
-        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/build
-    popd
+BuildAndPushPluginDNS() {
+    # Fetch Certbot version on 1st positional argument
+    CERTBOT_VERSION="$1"
+    # Fetch Certbot version on 2nd positional argument
+    PLUGIN="$2" #"dns-cloudflare"
+    # Fetch Docker repo name on 3rd positional argument
+    DOCKER_REPO="$3" 
+    # Build certbot core Docker image
+    docker build \
+        --build-arg CERTBOT_VERSION="${CERTBOT_VERSION}" \
+        --build-arg DOCKER_REPO="${DOCKER_REPO}" \
+        --build-arg PLUGIN_NAME="${PLUGIN}" \
+        -f source/plugin-armv7l/Dockerfile \
+        -t "${DOCKER_REPO}-${PLUGIN}:v${CERTBOT_VERSION}" \
+        .
+    docker push "${DOCKER_REPO}-${PLUGIN}:v${CERTBOT_VERSION}"
+    docker tag  "${DOCKER_REPO}-${PLUGIN}:v${CERTBOT_VERSION}" "${DOCKER_REPO}-${PLUGIN}:latest"
+    docker push "${DOCKER_REPO}-${PLUGIN}:latest"
 }
 
-WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+CleanUp() {
+    DOCKER_REPO="$1" 
+    docker rmi $(docker images --filter=reference="${DOCKER_REPO}*")
+}
 
-CERTBOT_VERSION="$1"
+if [[ -z $CERTBOT_VERSION ]]; then CERTBOT_VERSION=$(curl -s https://api.github.com/repos/certbot/certbot/releases/latest | grep -oP '"tag_name": "v\K(.*)(?=")'); fi
+if [[ -z $DOCKER_REPO ]]; then DOCKER_REPO="kitos9112/certbot-armv7l"; fi
 
 # Step 1: Certbot core Docker
-Build "certbot/certbot" "$CERTBOT_VERSION" "$WORK_DIR/core"
+BuildAndPushCore "${CERTBOT_VERSION}" "${DOCKER_REPO}"
 
-# Step 2: Certbot dns plugins Dockers
-CERTBOT_PLUGINS_DOCKER_REPOS=(
-    "certbot/dns-dnsmadeeasy"
-    "certbot/dns-dnsimple"
-    "certbot/dns-ovh"
-    "certbot/dns-cloudflare"
-    "certbot/dns-cloudxns"
-    "certbot/dns-digitalocean"
-    "certbot/dns-google"
-    "certbot/dns-luadns"
-    "certbot/dns-nsone"
-    "certbot/dns-rfc2136"
-    "certbot/dns-route53"
-    "certbot/dns-gehirn"
-    "certbot/dns-linode"
-    "certbot/dns-sakuracloud"
+# Step 2: Certbot dns plugins Docker
+
+CERTBOT_PLUGINS=(
+    "dns-dnsmadeeasy"
+    "dns-dnsimple"
+    "dns-ovh"
+    "dns-cloudflare"
+    "dns-cloudxns"
+    "dns-digitalocean"
+    "dns-google"
+    "dns-luadns"
+    "dns-nsone"
+    "dns-rfc2136"
+    "dns-route53"
+    "dns-gehirn"
+    "dns-linode"
+    "dns-sakuracloud"
 )
 
-for DOCKER_REPO in "${CERTBOT_PLUGINS_DOCKER_REPOS[@]}"; do
-    Build "${DOCKER_REPO}" "$CERTBOT_VERSION" "$WORK_DIR/plugin"
+for CERTBOT_PLUGIN in "${CERTBOT_PLUGINS[@]}"; do
+    BuildAndPushPluginDNS "${CERTBOT_VERSION}" "${CERTBOT_PLUGIN}" "${DOCKER_REPO}"
 done
 
-Cleanup
+# Step 3: Clean up all built images
+CleanUp "$DOCKER_REPO"
+
+
+
